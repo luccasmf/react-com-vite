eb20e36bc2acabfa96a2eb1d9bd59ebef783286c
Luccas M. Fonseca <luccas_mf@live.com>
2023-03-30 00:38:34 -0300

    gAdding MUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,9 +19,13 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button
+          variant="outlined"
+          color="warning"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
-        </button>
+        </Button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
